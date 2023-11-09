e2eb12c90def4ee8be2dda5bcecdc759a7ee37c5
mwises <88902469+mwises@users.noreply.github.com>
2023-11-09 16:52:27 -0300

    template Home

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,6 +1,10 @@
+import * as React from 'react';
+import * as Styled from './styles';
 import { useEffect, useState } from 'react';
 import Header from '../../components/Header';
-import GridContent from '../../components/GridContent';
+import { Box } from '@mui/material';
+import CardButton from '../../components/CardButton';
+import CardComponent from '../../components/Card';
 
 export function Home() {
   const [data, setData] = useState([]);
@@ -8,21 +12,51 @@ export function Home() {
   useEffect(() => {
     const load = async () => {
       try {
-        const data = await fetch('http://homologacao3.azapfy.com.br/api/ps/metahumans');
+        const data = await fetch(
+          'http://homologacao3.azapfy.com.br/api/ps/metahumans',
+        );
         const json = await data.json();
-        setData(json.results);
+        setData(json);
       } catch {
         setData(undefined);
       }
-    }
+    };
     load();
-  }, [])
+    console.log(data);
 
+
+
+  }, []);
 
   return (
     <>
       <Header />
-      <GridContent />
+      <Box
+      sx={{
+        maxWidth: '1600px',
+        flexGrow: 1,
+        margin: '6px auto',
+        padding: '8px 24px',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: ['column', 'row', 'row', 'row', 'row'],
+          alignItems: ['center', 'normal', 'normal', 'normal', 'normal'],
+          gap: ['24px', 0, 0, 0, 0]
+        }}
+      >
+        <Box>
+          <CardButton />
+        </Box>
+        <Styled.Grid>
+          {data.map((data) => (
+            <CardComponent key={data.id} src={data.images.md} name={data.name} power={data.powerstats.power} />
+          ))}
+        </Styled.Grid>
+      </Box>
+    </Box>
     </>
   );
 }
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,3 +1,24 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Grid = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    gap: ${theme.spacings.large};
+    counter-reset: grid-counter;
+    margin: 0 10px;
+
+    @media ${theme.media.lteMax} {
+      grid-template-columns: repeat(5, 1fr);
+    }
+    @media ${theme.media.lteMediumBig} {
+      grid-template-columns: repeat(3, 1fr);
+    }
+    @media ${theme.media.lteSmall} {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @media ${theme.media.lteXSmall} {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  `}
+`;
